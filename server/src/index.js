af69505f3f35245d2a8c2d612b416141083446da
FedMG <federico.gonzalia@gmail.com>
2022-12-15 22:11:51 +0000

    Create: connection to db in /index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,26 @@
 import express from 'express'
+import morgan from 'morgan'
+
+import connectDB from './db/connect.js'
+import products from './routes/products.js'
 
 const app = express()
 
 app.use(express.json())
+app.use(morgan('dev'))
+
+app.use('/api/v1/products', products)
+
+
+const PORT = 3000
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(PORT, () => console.log(`Listening port ${PORT}...`))
+    
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
