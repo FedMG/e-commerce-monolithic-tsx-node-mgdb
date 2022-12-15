7eb9041a747f14c9fe36dc458063c7e6f12a28bf
FedMG <federico.gonzalia@gmail.com>
2022-12-15 22:32:03 +0000

    Refactor: sortQuery.js

--- server/src/controllers/validators/sortQuery.js
+++ server/src/controllers/validators/sortQuery.js
@@ -1,10 +1,7 @@
 export const sortQuery = (result, sort) => {
   if (sort) {
     const values = splitAndJoin(sort);
-    result = result.sort(values);
-    
-  } else {
-    result = result.sort("createAt");
+    return result.sort(values);
   }
-  return result;
+  return result.sort("createAt");
 };
