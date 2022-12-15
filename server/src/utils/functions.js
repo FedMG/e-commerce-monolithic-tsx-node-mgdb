762ab59c2e40fcfa72c9632e54d691b0912f5d86
FedMG <federico.gonzalia@gmail.com>
2022-12-15 22:08:11 +0000

    Create: functions.js

---
+++ server/src/utils/functions.js
@@ -0,0 +1,4 @@
+export const splitAndJoin = (value) => value.split(',').join(' ')
+
+// const isFeatured = (prop) => prop === 'true' ? true : false
+// const regexFinder = (prop) => ({$regex: prop , $options: 'i'})
