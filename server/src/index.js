45939af3288d444448d4bc9c287e517a89e9108d
FedMG <federico.gonzalia@gmail.com>
2023-02-14 23:46:29 +0000

    feat: add rate limiting and set helmet

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,10 @@ import 'express-async-errors'
 
 import express from 'express'
 import morgan from 'morgan'
+
 import cors from 'cors'
+import helmet from 'helmet'
+import rateLimiter from 'express-rate-limit'
 
 import connectDB from './db/connect.js'
 import authenticateUser from './middleware/authentication.js'
@@ -14,16 +17,25 @@ import products from './routes/products.js'
 import notFound from './middleware/notFound.js'
 import errorHandler from './middleware/errorHandler.js'
 
+const app = express()
+
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
+
 const corsConfig = {
   origin: ['https://ek5sqy-3000.preview.csb.app'],
   optionsSuccessStatus: StatusCodes.OK
 }
 
-const app = express()
-
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(cors(corsConfig))
+app.use(helmet())
 
 app.use('/api/v1/auth', session)
 app.use('/api/v1/products', authenticateUser, products)
