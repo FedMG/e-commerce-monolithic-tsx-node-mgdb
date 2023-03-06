a276e72d128d90ca677b52b130f23fe3deb82bfd
FedMG <federico.gonzalia@gmail.com>
2023-03-06 03:21:21 +0000

    feat: add new route to handle request to get only images

--- server/src/routes/products.js
+++ server/src/routes/products.js
@@ -1,4 +1,5 @@
 import express from 'express'
+
 import {
   getAllProducts,
   createProduct,
@@ -8,8 +9,10 @@ import {
 } from '../controllers/products.js'
 import { getAllCategories } from '../controllers/productCategories.js'
 import { getAllBrands } from '../controllers/productBrands.js'
+import { getImage } from '../controllers/productImage.js'
 
 import multer from 'multer'
+
 const upload = multer({ storage: multer.memoryStorage() })
 
 const router = express.Router()
@@ -17,6 +20,7 @@ const router = express.Router()
 router.route('/').get(getAllProducts).post(upload.single('image'), createProduct)
 router.route('/categories').get(getAllCategories)
 router.route('/brands').get(getAllBrands)
+router.route('/:id/image').get(getImage)
 router.route('/:id').get(getProduct).delete(deleteProduct).patch(upload.single('image'), updateProduct)
 
 export default router
