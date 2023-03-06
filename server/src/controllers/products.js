1d623fd6503d12e68a36d303a835637bf7d84596
FedMG <federico.gonzalia@gmail.com>
2023-03-06 03:20:25 +0000

    feat: update createQuery function used in getAllProducts controller

--- server/src/controllers/products.js
+++ server/src/controllers/products.js
@@ -1,100 +1,101 @@
-import Product from "../models/product.js";
+import Product from '../models/product.js'
 
-import { StatusCodes } from "http-status-codes";
-import { NotFoundError, BadRequestError } from "../errors/customTypes.js";
+import { StatusCodes } from 'http-status-codes'
+import { NotFoundError, BadRequestError } from '../errors/customTypes.js'
 
-import { createQuery } from "./validators/createQuery.js";
+import { createQuery } from './validators/createQuery.js'
 import {
   uploadFileToTheCloud,
   updateFileInTheCloud,
-  deleteFileInTheCloud,
-} from "../cloud/controllers.js";
+  deleteFileInTheCloud
+} from '../cloud/controllers.js'
 
 const getAllProducts = async (req, res) => {
-  const { name, brand, category, sort, fields, numFilter, page, limit } =
-    req.query;
+  const { name, brand, description, category, sort, fields, numFilter, page, limit } =
+    req.query
 
   const products = await createQuery({
     name,
     brand,
+    description,
     category,
     sort,
     fields,
     numFilter,
     page,
-    limit,
-  });
+    limit
+  })
 
-  res.status(StatusCodes.OK).json({ products, numHits: products.length });
-};
+  res.status(StatusCodes.OK).json({ products, numHits: products.length })
+}
 
 const createProduct = async (req, res) => {
-  const product = new Product({ ...req.body });
+  const product = new Product({ ...req.body })
 
-  if (!req.file) throw new BadRequestError("image is required.");
+  if (!req.file) throw new BadRequestError('image is required.')
 
-  const { buffer } = req.file;
-  product.image.src = await uploadFileToTheCloud(buffer);
-  product.save();
+  const { buffer } = req.file
+  product.image.src = await uploadFileToTheCloud(buffer)
+  product.save()
 
-  res.status(StatusCodes.CREATED).json({ product });
-};
+  res.status(StatusCodes.CREATED).json({ product })
+}
 
 const getProduct = async (req, res, next) => {
-  const { id: productId } = req.params;
-  const product = await Product.findOne({ _id: productId });
+  const { id: productId } = req.params
+  const product = await Product.findOne({ _id: productId })
 
   if (!product) {
-    throw new NotFoundError(`There is not a product with id ${productId}`);
+    throw new NotFoundError(`There is not a product with id ${productId}`)
   }
 
-  res.status(StatusCodes.OK).json({ product });
-};
+  res.status(StatusCodes.OK).json({ product })
+}
 
 const deleteProduct = async (req, res, next) => {
-  const { id: productId } = req.params;
-  const product = await Product.findOne({ _id: productId });
+  const { id: productId } = req.params
+  const product = await Product.findOne({ _id: productId })
 
   if (!product) {
-    throw new NotFoundError(`There is not a product with id ${productId}`);
+    throw new NotFoundError(`There is not a product with id ${productId}`)
   }
 
-  const secureURL = product.image.src;
-  await deleteFileInTheCloud(secureURL);
-  await Product.deleteOne({ _id: productId });
+  const secureURL = product.image.src
+  await deleteFileInTheCloud(secureURL)
+  await Product.deleteOne({ _id: productId })
 
-  res.status(StatusCodes.OK).json({});
-};
+  res.status(StatusCodes.OK).json({})
+}
 
 const updateProduct = async (req, res, next) => {
   const { id: productId } = req.params
   const product = await Product.findOne({ _id: productId })
-  
+
   if (!product) {
-    throw new NotFoundError(`There is not a product with id ${productId}`);
+    throw new NotFoundError(`There is not a product with id ${productId}`)
   }
-  
+
   if (req.file) {
     const secureURL = product.image.src
     const { buffer } = req.file
-    
+
     product.set({
       image: {
-        src: await updateFileInTheCloud(secureURL, buffer),
-      },
-    });
+        src: await updateFileInTheCloud(secureURL, buffer)
+      }
+    })
   }
-  
+
   product.set({ ...req.body })
   product.save({ runValidators: true })
-  
+
   res.status(StatusCodes.OK).json({})
-};
+}
 
 export {
   getAllProducts,
   createProduct,
   getProduct,
   deleteProduct,
-  updateProduct,
-};
+  updateProduct
+}
