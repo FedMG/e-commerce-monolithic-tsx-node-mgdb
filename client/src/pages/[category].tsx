a8715d47647e53d2a4dd7c34d8d7085bee0bdf6c
FedMG <federico.gonzalia@gmail.com>
2023-03-20 06:20:43 +0000

    refactor: map method in category page component

--- client/src/pages/[category].tsx
+++ client/src/pages/[category].tsx
@@ -3,26 +3,34 @@ import { Grid } from "@mantine/core";
 import { ProductsCard } from "@/components/cards";
 import { getEndpoint } from "./api/utils";
 
-import { CategoryProps, CategoryServerSideProps } from "additional";
+import { CategoryProps, CategoryServerSideProps, Products } from "additional";
 import { FC, ReactElement } from "react";
 
 import { VALID_DOMAIN } from "src/environment";
 
-const Category: FC<CategoryProps> = ({ products }): ReactElement => {
+
+const renderProducts: FC<Products> = ({
+  name,
+  price,
+  rating,
+  image,
+  ...element
+}): ReactElement => {
+  return (
+    <Grid.Col span={4} sm={3} md={3} key={element["_id"]}>
+      <ProductsCard element={{ name, price, rating, image }} />
+    </Grid.Col>
+  );
+};
+
+const Category: React.FC<CategoryProps> = ({ products }) => {
   return (
     <Grid gutter={6} gutterXs={16} gutterSm={20} gutterXl={24}>
       <Grid.Col span={0} lg={2}>
         {<h3>Filters</h3>}
       </Grid.Col>
-      <Grid.Col span={12} lg={10} display="flex" style={{ flexWrap: "wrap" }}>        
-        {products.map(({ name, price, rating, ...element}) => {
-          return (
-            <Grid.Col span={4} sm={3} md={3} key={element["_id"]}>
-              <ProductsCard element={{ name, price, rating}} />
-            </Grid.Col>
-          );
-        })}
-        
+      <Grid.Col span={12} lg={10} display="flex" style={{ flexWrap: "wrap" }}>
+        {products.map(renderProducts)}
       </Grid.Col>
     </Grid>
   );
