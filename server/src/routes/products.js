6bdd7e5ddcec92cdfbb2e3f141102d66522e6412
FedMG <federico.gonzalia@gmail.com>
2023-03-28 00:25:57 +0000

    feat: add new /:category/discounts route with controller

--- server/src/routes/products.js
+++ server/src/routes/products.js
@@ -11,6 +11,7 @@ import {
   updateProduct
 } from '../controllers/products.js'
 import { getAllCategories } from '../controllers/productCategories.js'
+import { getAllDiscounts } from '../controllers/productDiscounts.js'
 import { getAllBrands } from '../controllers/productBrands.js'
 import { getImage } from '../controllers/productImage.js'
 
@@ -19,6 +20,7 @@ const router = express.Router()
 
 router.route('/').get(getAllProducts).post(authenticateUser, upload.single('image'), createProduct)
 router.route('/categories').get(getAllCategories)
+router.route('/:category/discounts').get(getAllDiscounts)
 router.route('/brands').get(getAllBrands)
 router.route('/:id/image').get(getImage)
 router.route('/:id').get(getProduct).delete(authenticateUser, deleteProduct).patch(authenticateUser, upload.single('image'), updateProduct)
