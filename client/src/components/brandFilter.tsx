279dc88e7db95d2334c313eb709d3787334a6210
FedMG <federico.gonzalia@gmail.com>
2023-03-31 19:54:39 +0000

    fix: add missing conditional to check wether there are or not brands

--- client/src/components/brandFilter.tsx
+++ client/src/components/brandFilter.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-import { setUpperCase } from "@/utils";
+import { isThereProduct, setUpperCase } from "@/utils";
 import type { CategoryBrandsFilterProps } from "additional";
 
 
@@ -32,7 +32,7 @@ export const CategoryBrandFilter: React.FC<CategoryBrandsFilterProps> = ({ brand
   return (
     <div className='flex flex-col'>
       <span className='text-gray-700 font-semibold mb-2'>Brands</span>
-      {brands.map((name) => (
+      {isThereProduct(brands) && brands.map((name) => (
         <div key={name} className='flex align-center'>
           <input type="checkbox" id={name} name='brand' value={name} onChange={(e) => handleChange(name, e.target.checked)} className='mr-3' checked={selecteds.has(name)} />
           <label htmlFor={name} className='text-gray-700'>{setUpperCase(name)}</label>
