4663ba2424f20b1c7e82556e81b514e763143773
FedMG <federico.gonzalia@gmail.com>
2023-03-31 20:04:57 +0000

    fix: bug in dynamic routes for incorrect references of Image
    bug details: src of Image component had missing / to begin of the path, it triggrs a conflict with parameters in dynamic routes

--- client/src/components/layout.tsx
+++ client/src/components/layout.tsx
@@ -1,9 +1,11 @@
 import Head from 'next/head'
-import { RootLayout } from 'additional'
 
 import { HeaderAction } from './structure/Header'
 import { FooterLinks } from "./structure/Footer";
+
 import { footer, headerLinks } from '@/refs';
+
+import { RootLayout } from 'additional'
 
 export const Layout: React.FC<RootLayout> = ({ children, title }) => {
   return (
@@ -11,7 +13,7 @@ export const Layout: React.FC<RootLayout> = ({ children, title }) => {
       <Head>
         <meta name='description' content='An E-commerce project' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='e-cart.ico' />
+        <link rel='icon' href='/e-cart.ico' />
         <title>{title}</title>
       </Head>
       <HeaderAction links={headerLinks} />
