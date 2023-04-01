3bb5da830fd801b487e665511913ec2c3c71918b
FedMG <federico.gonzalia@gmail.com>
2023-04-01 05:47:55 +0000

    fix: add conditional to check whether there are discounts or not

--- client/src/components/discountFilter.tsx
+++ client/src/components/discountFilter.tsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
 
-import { isNumber } from "@/utils";
+import { isNumber, isThereProduct } from "@/utils";
 import type { CategoryDiscountsFilterProps } from "additional";
-
 
 export const CategoryDiscountFilter: React.FC<CategoryDiscountsFilterProps> = ({ onChange, discounts, currentCategory }) => {
   const [selecteds, setSelected] = useState<Set<number>>(() => new Set())
@@ -33,7 +32,7 @@ export const CategoryDiscountFilter: React.FC<CategoryDiscountsFilterProps> = ({
   return (
     <div className='flex flex-col'>
       <span className='text-gray-700 font-semibold mb-2'>Discounts</span>
-      {discounts.map((discount, id) => (
+      {isThereProduct(discounts) && discounts.map((discount, id) => (
         <div key={id} className='flex align-center'>
           <input type="checkbox" id={"" + discount} name='discount' value={discount} onChange={(e) => handleSelectedOptions(discount, e.target.checked)} className='mr-3' checked={selecteds.has(discount)} />
           <label htmlFor={"" + discount} className='text-gray-700'>-{discount} Off</label>
