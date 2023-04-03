e079fd345394bd19cbf5b859f29f0736b11c54ec
FedMG <federico.gonzalia@gmail.com>
2023-04-03 07:31:08 +0000

    feat: update styles in Footer component

--- client/src/components/structure/Footer.tsx
+++ client/src/components/structure/Footer.tsx
@@ -2,10 +2,9 @@ import { isArrayOfObjects } from "@/utils"
 import type { FooterLinkIconProps, FooterProps } from "additional"
 
 
-const FooterLinkIcon: React.FC<FooterLinkIconProps> = ({ path = '#', size = 6, d, alt }) => (
-  <a href={path} className="text-white hover:bg-gray-50 hover:text-black p-1.5 rounded-md active:bg-black active:text-white">
+const FooterLinkIcon: React.FC<FooterLinkIconProps> = ({ path = '#', d, alt }) => (
+  <a href={path} className={`w-full h-full max-h-[35px] max-w-[35px] text-white hover:bg-gray-50 hover:text-black p-1 rounded-md active:bg-black active:text-white`}>
     <svg
-      className={`w-${size} h-${size}`}
       fill="currentColor"
       viewBox="0 0 24 24"
       aria-hidden="true"
@@ -30,8 +29,8 @@ const FooterColumnLinks: React.FC<FooterProps> = ({ links }) => {
         {links.map(({ name, path, links }) => {
           if (links) {
             return (
-              <div className='px-10 py-2 inline-block' key={name}>
-                <h2 className="mb-6 text-lg font-bold font-sans text-gray-900 uppercase dark:text-white">
+              <div className='px-8 py-2 inline-block' key={name}>
+                <h2 className="mb-4 text-lg font-bold font-sans text-gray-900 dark:text-white">
                   {name}
                 </h2>
                 <ul>
@@ -41,8 +40,8 @@ const FooterColumnLinks: React.FC<FooterProps> = ({ links }) => {
             )
           }
           return (
-            <li key={name} className="pb-4 text-md md:text-lg font-medium hover:underline text-gray-600 dark:text-gray-50">
-              <a href={path}>
+            <li key={name} className="pb-3 text-md font-medium text-gray-600 dark:text-gray-50">
+              <a href={path} className='hover:underline'>
                 {name}
               </a>
             </li>
@@ -54,13 +53,13 @@ const FooterColumnLinks: React.FC<FooterProps> = ({ links }) => {
 
 
 const CopyrightAndSocialIcons = () => (
-  <div className="px-8 w-full bg-black flex flex-col sm:flex-row space-around items-center mt-8 py-8 border-t dark:border-slate-400 border-gray-200">
+  <div className="p-8 mt-5 w-full bg-black flex flex-col sm:flex-row space-around items-center border-t dark:border-slate-400 border-gray-200">
     <p className="text-sm text-white sm:w-full">
       © 2023 AstraShop™. Copyright example.
     </p>
     <div className="flex flex-nowrap justify-end gap-0 space-x-2 my-4 sm:w-full sm:my-0">
-      <FooterLinkIcon path='#' size={8} alt='LinkedIn page' d='M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z' />
-      <FooterLinkIcon path='#' size={8} alt='GitHub page' d='M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z' />       
+      <FooterLinkIcon path='#' alt='LinkedIn page' d='M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z' />
+      <FooterLinkIcon path='#' alt='GitHub page' d='M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z' />
     </div>
   </div>
 )
@@ -68,8 +67,8 @@ const CopyrightAndSocialIcons = () => (
 
 export const Footer: React.FC<FooterProps> = ({ links }) => {
   return (
-    <footer className="mt-120 bg-gray-50 dark:bg-slate-600 border-t dark:border-slate-500 border-gray-200">
-      <div className="hidden sm:flex w-full justify-around">
+    <footer className="mt-20 bg-gray-100 dark:bg-slate-600 border-t dark:border-slate-500 border-gray-200">
+      <div className="mt-5 hidden sm:flex w-full justify-around">
         <FooterColumnLinks links={links} />
       </div>
       <CopyrightAndSocialIcons />
