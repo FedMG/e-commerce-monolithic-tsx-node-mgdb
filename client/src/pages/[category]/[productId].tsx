0e3645a2021985c9500c8b1037600b040caa3626
FedMG <federico.gonzalia@gmail.com>
2023-04-28 01:05:45 +0000

    refactor: svg element of button in productId page

--- client/src/pages/[category]/[productId].tsx
+++ client/src/pages/[category]/[productId].tsx
@@ -10,11 +10,13 @@ import { ProductDiscountPrice } from '@/components/productDiscount'
 import { ProductClothingSizes } from '@/components/productClothingSizes'
 import { ProductClothingColors } from '@/components/productClothingColors'
 import { ProductsNumberInput } from '@/components/productNumberInput'
+import { SVGElement, PathElement } from '@/components/svgElements'
 import { ProductButton } from '@/components/productButton'
 
 import { setUpperCase } from '@/utils'
 import { getEndpoint } from '../api/utils'
 import { productColors } from '@/refs'
+
 
 import type { GetServerSidePropsContext, GetServerSidePropsResult } from 'next'
 import type { Params } from 'next/dist/shared/lib/router/utils/route-matcher'
@@ -76,16 +78,10 @@ const ProductMainInfo: FC<Pick<ProductObject, 'name' | 'price' | 'rating' | 'dis
         <div className='flex flex-col pt-2 gap-2 md:gap-3 justify-end h-full'>
           <ProductButton name='Buy now' />
           <ProductButton name='Add to cart'>
-            <svg
-              aria-hidden='true'
-              className='w-5 h-5 mr-2 -ml-1'
-              fill='currentColor'
-              viewBox='0 0 20 20'
-              xmlns='http://www.w3.org/2000/svg'
-            >
-              <title className='sr-only'>Cart icon of the button</title>
-              <path d='M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z' />
-            </svg>
+            <SVGElement className='w-5 h-5 mr-2 -ml-1' fillCurrent>
+              <span className='sr-only'>Cart icon of the button</span>
+              <PathElement d='M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z' />
+            </SVGElement>
           </ProductButton>
         </div>
       </div>
