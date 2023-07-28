c20b990734bac1cfa1ab3541d97b4646258d1dcb
FedMG <75050231+FedMG@users.noreply.github.com>
2023-07-28 11:26:16 +0000

    fix: remove temporally cartProvider and related cart things

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,12 +1,7 @@
-import { CartProvider } from '@/contexts/cartContext'
-import '@/styles/globals.css'
 import type { AppPropsWithLayout } from '@/next-pages'
+import '@/styles/globals.css'
 
 export default function App ({ Component, pageProps }: AppPropsWithLayout): React.ReactNode {
   const getLayout = Component.getLayout ?? ((page) => page)
-  return (
-    <CartProvider>
-      {getLayout(<Component {...pageProps} />, pageProps)}
-    </CartProvider>
-  )
+  return getLayout(<Component {...pageProps} />, pageProps)
 }
