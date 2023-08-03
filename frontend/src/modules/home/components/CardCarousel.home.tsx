14e31d8e2c2b3e7460c7a029ff102cecce84543f
FedMG <75050231+FedMG@users.noreply.github.com>
2023-08-03 23:26:29 +0000

    feat: add and implement staggering effect in ProductCards
    add: StaggeringWrapper component to wrap productCard component at /CardCarousel file
    
    add: useStaggeringDelay custom hook

--- frontend/src/modules/home/components/CardCarousel.home.tsx
+++ frontend/src/modules/home/components/CardCarousel.home.tsx
@@ -9,6 +9,7 @@ import { sanatize } from '@/utils'
 
 import type { BaseComponentProps } from '@/schemas'
 import type { ProductCard } from '@/models'
+import { StaggeringWrapper } from '@/components/StaggeringWrapper'
 
 type CarouselProductCard = Omit<ProductCard, 'createdAt' | 'category'>
 export type CardCarouselProps = Pick<BaseComponentProps, 'children'> & {
@@ -30,10 +31,9 @@ export const CardCarousel: React.FC<CardCarouselProps> = ({ section, children })
         <CardCarouselTitle section={category}>{children}</CardCarouselTitle>
         <Carousel className='pb-4 pt-2'>
           {RANGE.map((_, id) => (
-            <CardCarouselSkeleton
-              key={id}
-              className='w-[150px] max-h-[230px] sm:w-[170px] sm:max-h-[260px] md:w-[190px] md:max-h-[290px] lg:w-[212px] lg:max-h-[330px]'
-            />
+            <StaggeringWrapper time={id} key={id}>
+              <CardCarouselSkeleton className='w-[150px] max-h-[230px] sm:w-[170px] sm:max-h-[260px] md:w-[190px] md:max-h-[290px] lg:w-[212px] lg:max-h-[330px]' />
+            </StaggeringWrapper>
           ))}
         </Carousel>
       </div>
@@ -44,12 +44,13 @@ export const CardCarousel: React.FC<CardCarouselProps> = ({ section, children })
     <div className='pt-4'>
       <CardCarouselTitle section={category}>{children}</CardCarouselTitle>
       <Carousel className='items-center pb-4 pt-2'>
-        {items.map(({ id, ...rest }) => (
-          <ProductsCard
-            key={id}
-            element={{ id, ...rest, category }}
-            className='w-[150px] max-h-[230px] sm:w-[170px] sm:max-h-[260px] md:w-[190px] md:max-h-[290px] lg:w-[220px] lg:max-h-[330px]'
-          />
+        {items.map(({ id, ...rest }, index) => (
+          <StaggeringWrapper time={index} key={id}>
+            <ProductsCard
+              element={{ id, ...rest, category }}
+              className='w-[150px] max-h-[230px] sm:w-[170px] sm:max-h-[260px] md:w-[190px] md:max-h-[290px] lg:w-[220px] lg:max-h-[330px]'
+            />
+          </StaggeringWrapper>
         ))}
       </Carousel>
     </div>
