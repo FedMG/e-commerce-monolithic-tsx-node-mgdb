b8c16f61eef27d1c39f7928246a38a9fb2e616b1
FedMG <75050231+FedMG@users.noreply.github.com>
2023-11-03 12:15:21 +0000

    feat: add brands logo and refactor payment logos

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -1,1 +1,4 @@
 export * from './Icons'
+
+export { BRANDS_LOGO } from './BrandsLogo.assets'
+export { PAYMENTS_LOGO } from './PaymentMethodsLogo.assets'
--- frontend/src/modules/cart/components/PaymentInformation.cart.tsx
+++ frontend/src/modules/cart/components/PaymentInformation.cart.tsx
@@ -1,20 +1,12 @@
-import {
-  BanknotesIcon,
-  BitcoinIcon,
-  CreditCardIcon,
-  EthereumIcon,
-  MastercardIcon,
-  MercadoPagoIcon,
-  VisaIcon
-} from '@/assets'
+import { BanknotesIcon, CreditCardIcon, PAYMENTS_LOGO } from '@/assets'
 
 export const PaymentInformation = () => (
   <>
     <div className='pt-2 pb-6'>
       <h5 className='text-gray-700 text-md font-medium pb-2'>Credit Card</h5>
       <div className='pl-0 flex flex-wrap items-center gap-4'>
-        <VisaIcon />
-        <MastercardIcon />
+        {PAYMENTS_LOGO['visa']}
+        {PAYMENTS_LOGO['mastercard']}
         <CreditCardIcon />
       </div>
     </div>
@@ -22,8 +14,8 @@ export const PaymentInformation = () => (
     <div className='pt-2 pb-6'>
       <h5 className='text-gray-700 text-md font-medium pb-2'>Debit Card</h5>
       <div className='pl-0 flex flex-wrap items-center gap-4'>
-        <VisaIcon />
-        <MastercardIcon />
+        {PAYMENTS_LOGO['visa']}
+        {PAYMENTS_LOGO['mastercard']}
         <CreditCardIcon />
       </div>
     </div>
@@ -31,15 +23,16 @@ export const PaymentInformation = () => (
     <div className='pt-2 pb-6'>
       <h5 className='text-gray-700 text-md font-medium pb-2'>Cryptocurrencies</h5>
       <div className='pl-0 flex flex-wrap items-center gap-4'>
-        <BitcoinIcon />
-        <EthereumIcon />
+        {PAYMENTS_LOGO['bitcoin']}
+        {PAYMENTS_LOGO['ethereum']}
+
       </div>
     </div>
 
     <div className='pt-2 pb-6'>
       <h5 className='text-gray-700 text-md font-medium pb-2'>Virtual Wallet</h5>
       <div className='pl-0 flex flex-wrap items-center gap-4'>
-        <MercadoPagoIcon />
+        {PAYMENTS_LOGO['mercado pago']}
       </div>
     </div>
 
