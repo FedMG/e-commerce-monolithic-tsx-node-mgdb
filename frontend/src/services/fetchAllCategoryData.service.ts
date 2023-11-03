f4ddf022510e7c58af891c274ebd611d83ff8f6d
FedMG <75050231+FedMG@users.noreply.github.com>
2023-11-03 11:24:56 +0000

    fix: incorrect string type to Brands type

--- frontend/src/services/fetchAllCategoryData.service.ts
+++ frontend/src/services/fetchAllCategoryData.service.ts
@@ -1,11 +1,11 @@
 import { fetchAllProducts, fetchUniqueProductValues } from '@/services'
-import type { ProductCard } from '@/models'
+import type { Brands, ProductCard } from '@/models'
 
-type FetchResponse = [string[], number[], ProductCard[]]
+type FetchResponse = [Brands[], number[], ProductCard[]]
 
 export const fetchAllCategoryData = async (category: string, signal: AbortSignal): Promise<FetchResponse> => {
   return await Promise.all([
-    fetchUniqueProductValues<string>({ category, property: 'brand', signal }),
+    fetchUniqueProductValues<Brands>({ category, property: 'brand', signal }),
     fetchUniqueProductValues<number>({ category, property: 'discount', signal }),
     fetchAllProducts({ category, signal })
   ])
